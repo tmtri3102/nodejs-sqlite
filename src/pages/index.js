3b2d759636f1982c02e79c1cf8de403142e06de9
tmtri3102 <tmtri310251@gmail.com>
2025-02-02 23:21:31 +0700

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,39 @@
 import styles from "../styles/Home.module.css";
 import { getStudents } from "./mock-data/data";
 import Link from "next/link";
+import Layout from "./layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <table className={styles.table}>
-          <thead>
-            <tr className={styles.tr}>
-              <th className={styles.th}>ID</th>
-              <th className={styles.th}>Name</th>
-              <th className={styles.th}>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getStudents().map((student) => (
-              <tr className={styles.tr} key={student.id}>
-                <td className={styles.td}>{student.id}</td>
-                <td className={styles.td}>{student.name}</td>
-                <td className={styles.td}>
-                  <Link href={`/student/${encodeURIComponent(student.id)}`}>
-                    Show
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
-    </div>
+      <Layout>
+        <div>Homepage</div>
+      </Layout>
+    // <div className={styles.container}>
+    //
+    //   <main className={styles.main}>
+    //     <table className={styles.table}>
+    //       <thead>
+    //         <tr className={styles.tr}>
+    //           <th className={styles.th}>ID</th>
+    //           <th className={styles.th}>Name</th>
+    //           <th className={styles.th}>Action</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         {getStudents().map((student) => (
+    //           <tr className={styles.tr} key={student.id}>
+    //             <td className={styles.td}>{student.id}</td>
+    //             <td className={styles.td}>{student.name}</td>
+    //             <td className={styles.td}>
+    //               <Link href={`/student/${encodeURIComponent(student.id)}`}>
+    //                 Show
+    //               </Link>
+    //             </td>
+    //           </tr>
+    //         ))}
+    //       </tbody>
+    //     </table>
+    //   </main>
+    // </div>
   );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,36 @@
+import styles from "../styles/Login.module.css";
+import { useRouter } from "next/router";
+
+export default function Login() {
+    const router = useRouter ();
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.formContent}>
+                <h2>Login</h2>
+                <form>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        id="login"
+                        name="login"
+                        placeholder="login"
+                    />
+                    <input
+                        className={styles.input}
+                        type="text"
+                        id="password"
+                        name="login"
+                        placeholder="password"
+                    />
+                    <button
+                        type="button"
+                        className={styles.button}
+                        onClick={() => router.push ( "/" )}
+                    >
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}

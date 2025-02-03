db780f66f03c0ff95b29ce4ad092588d936a7f3c
tmtri3102 <tmtri310251@gmail.com>
2025-02-03 08:13:59 +0700

    Weather

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
 import styles from "../styles/Home.module.css";
 import { getStudents } from "./mock-data/data";
 import Link from "next/link";
+import Image from "next/image";
 
 export async function getStaticProps() {
-    const res = await fetch ( "https://api.api-ninjas.com/v1/covid19?country=vietnam" );
-    const data = await res.json ();
+    // const res = await fetch ( "https://api.api-ninjas.com/v1/covid19?country=vietnam" );
+    const res = await fetch ( "https://api.openweathermap.org/data/2.5/weather?lat=21.028511&lon=105.804817&appid=30de923456283628da276346257b5d19" );
+    const repo = await res.json ();
 
-    return {props: {data}}
+    return {props: {repo}}
 }
 
-export default function Home({data}) {
+export default function Home({repo}) {
+    let iconCode = repo.weather[0].icon;
     return (
-        console.log ( data )
+        <>
+            <table>
+                <thead></thead>
+                <tbody>
+                <tr>
+                    <td className="city">{repo.name}</td>
+                    <td className="temperature">{Math.round(repo.main.temp - 273.15)}<span className="degree"> ℃</span></td>
+                    <td className="details">
+                        Weather: <span style={{fontWeight: "bold"}}>{repo.weather[0].description}</span><br/>
+                        Humidity: <span style={{fontWeight: "bold"}}>{repo.main.humidity} %</span><br/>
+                        Wind speed: <span style={{fontWeight: "bold"}}>{repo.wind.speed} m/s</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </>
         // <div>
         //     <h1>Thông tin COVID-19 tại Việt Nam</h1>
         //

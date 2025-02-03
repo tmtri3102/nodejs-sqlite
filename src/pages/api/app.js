d1aac2301b6c9600229ab488960876b16cd50fb3
tmtri3102 <tmtri310251@gmail.com>
2025-02-03 09:57:36 +0700

    delete folders and files in api folder

--- src/pages/api/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const apiMocker = require("connect-api-mocker");
-
-const port = 9000;
-const app = express();
-
-app.use("/api", apiMocker("mock-api"));
-
-app.listen(port);
